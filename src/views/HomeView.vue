b186c39e3baa50b56afb5e0771b08cdd2bcbc930
Van <84937597+vandalit@users.noreply.github.com>
2024-10-12 17:17:16 -0300

    implementando home con data de API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
   <div>
     <h1>Bienvenidos a la Tienda de Zapatos</h1>
-    <!-- Carrusel y secciones -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div
+          class="carousel-item"
+          v-for="(zapato, index) in zapatos"
+          :class="{ active: index === 0 }"
+          :key="zapato.titulo"
+        >
+          <img :src="zapato.imagen" class="d-block w-100" :alt="zapato.titulo">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>{{ zapato.titulo }}</h5>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'HomeView'
-  };
+import apiService from '@/services/apiService';
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      zapatos: []
+    };
+  },
+  mounted() {
+    apiService.getHomeData().then(response => {
+      this.zapatos = response.data.mejoreszapatos;
+    });
+  }
+};
 </script>
 
 <style scoped>
-  h1 {
-    color: $primary-color;
-  }
+h1 {
+  color: $primary-color;
+}
 </style>
