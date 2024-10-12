41e7de71f692311c952e47b42d76bcce133e25ac
Van <84937597+vandalit@users.noreply.github.com>
2024-10-12 17:15:42 -0300

    json and api

---
+++ src/services/apiService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: '/data',
+  withCredentials: false
+});
+
+export default {
+  getHomeData() {
+    return apiClient.get('/home.json');
+  },
+  getProductosData() {
+    return apiClient.get('/productos.json');
+  }
+};
